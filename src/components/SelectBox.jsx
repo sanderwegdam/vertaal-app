69c2b411e2f3fd163e8d2114359994f4fbbd6da1
Sander Wegdam <sanderwegdam@gmail.com>
2022-07-08 20:04:11 +0200

    'deloy'

--- src/components/SelectBox.jsx
+++ src/components/SelectBox.jsx
@@ -1,9 +1,10 @@
+
 export const SelectBox = ({ id, select }) => {
     return (
         <>
             <div className="select">
-                <select id={id} onChange={select} defaultValue={'default'}>
-                    <option value="default">Select Language</option>
+                <select className="round" id={id} onChange={select} defaultValue={'default'}>
+                    <option value="default">Selecteer taal</option>
                     <option value="nl">Nederlands</option>
                     <option value="en">Engels</option>
                     <option value="de">Duits</option>
